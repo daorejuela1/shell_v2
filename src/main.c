afaa8ac85ec773c6b92255075743a2bf27006cbf
daorejuela1 <daorejuela1@outlook.com>
2021-01-09 20:45:46 +0000

    fixed order to reset line after startup file reading

--- src/main.c
+++ src/main.c
@@ -20,6 +20,7 @@ int main(int argc, char *argv[])
 	c_args.line = &line, c_args.argv = argv, c_args.aliases = NULL;
 	c_args.counter = &counter, c_args.start = &start, c_args.status = &status;
 	startup_file(&c_args);
+	c_args.line = &line;
 	if (argc > 1)
 		status = open_file(&c_args, argv[1]);
 	else if (isatty(STDIN_FILENO))
@@ -48,7 +49,6 @@ int main(int argc, char *argv[])
 			status = (_strcmp(line, "env\n") == 0) ? print_env() : new_pro(&c_args);
 		}
 	}
-	free(line), free_env();
-	alias_free(c_args);
+	free(line), free_env(), alias_free(c_args);
 	return (status);
 }
